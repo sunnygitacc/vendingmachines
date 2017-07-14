c4c8136efdbd348b3db6ec2c2ec157fe5832c47b
gcf2 <Sunil.Kata@in.tesco.com>
2017-07-14 14:21:25 +0530

    Skeleton for the pom and the main class created

---
+++ src/main/java/com/target/vendingmachines/VendingMachine.java
@@ -0,0 +1,10 @@
+package com.target.vendingmachines;
+
+/**
+ * Created by Sunil Kata on 7/14/2017.
+ */
+public class VendingMachine {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the world of Vending Machines");
+    }
+}
